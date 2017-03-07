4e3db8874bac45ddfe2a082e8b9865afbbd0f6e5
Sergiy Yevtushenko <Sergiy.Yevtushenko@gmail.com>
2017-03-07 06:49:11 +0100

    Intermediate commit

--- main-site/src/Comments.js
+++ main-site/src/Comments.js
@@ -1,34 +1,5 @@
-import React from 'react';
-
-const containerStyle = {
-  backgroundColor: 'white',
-  color: 'black',
-  width: '100%',
-}
-
-const firstTitleStyle = {
-  fontSize: '28px',
-  verticalAlign: 'top',
-  align: 'left',
-}
-
-const secondTitleStyle = {
-  fontSize: '28px',
-  verticalAlign: 'top',
-  align: 'right',
-}
-
-const firstCommentStyle = {
-  fontSize: '24px',
-  position: 'relative',
-  left: '1%',
-}
-
-const secondCommentStyle = {
-  // fontSize: '24px',
-  // position: 'relative',
-  // right: '1%',
-}
+import Paper from 'material-ui/Paper';
+import React from 'react'
 
 const leftDividerStyle = {
   display: 'block',
@@ -37,77 +8,112 @@ const leftDividerStyle = {
   margin: '1em 0',
   padding: 0,
   width: '50%',
-  align: 'left',
+  align: 'left'
 }
 
 const rightDividerStyle = {
-  // display: 'block',
-  // border: 0,
-  // borderTop: '3px solid #000',
-  // margin: '1em 0',
-  // padding: 0,
-  // width: '50%',
-  // align: 'right',
-}
-
-const firstCommentTextStyle = {
-  position: 'relative',
-  left: '3%',
-  verticalAlign: 'top',
+  display: 'block',
+  border: 0,
+  borderTop: '3px solid #000',
+  margin: '1em 0',
+  padding: 0,
   width: '50%',
+  align: 'right'
 }
 
-const secondCommentTextStyle = {
-  // position: 'relative',
-  // right: '3%',
-  // verticalAlign: 'top',
-  // width: '50%',
+const containerStyle = {
+  backgroundColor: 'white',
+  color: 'black',
+  width: '100%',
+  fontSize: '24px',
+  lineHeight: '1.5em',
+  position: 'relative',
 }
 
-const CommentLeft = (props) => (
-  <div style={props.style} >
-    <hr style={props.dividerStyle}/>
-    <img src={props.icon} alt=""/><span style={props.titleStyle}>{props.title}</span>
-    <br/>
-    <div style={props.textStyle}>
-      {props.children}
-    </div>
-  </div>
-);
+const leftContainerStyle = {
+  align: 'left',
+  verticalAlign: 'top',
+  border: '1px solid black',
+}
 
-const CommentRight = (props) => (
-  <div style={props.style} >
-    <hr style={props.dividerStyle}/>
-    <span style={props.titleStyle}>{props.title}</span><img src={props.icon} alt=""/>
-    <br/>
-    <div style={props.textStyle}>
-      {props.children}
-    </div>
-  </div>
-);
+const rightContainerStyle = {
+  align: 'right',
+  verticalAlign: 'top',
+  border: '1px solid black',
+}
+
+const titleStyle = {
+  fontSize: '28px'
+}
+
+const cytingStyle = {
+  padding: '30px',
+  color: 'white',
+  backgroundColor: 'gray',
+  align: 'left',
+  whiteSpace: 'nowrap',
+  borderRadius: '15px',
+}
+
+//TODO: move styles to CSS, finish page
 
 const Comments = () => (
   <div style={containerStyle}>
-    <CommentLeft style={firstCommentStyle} 
-      title="Odbądź podróż po świecie Twoich znajomych"
-      titleStyle={firstTitleStyle}
-      icon="image/account-search.png"
-      dividerStyle={leftDividerStyle}
-      textStyle={firstCommentTextStyle}
-      >
-      Widząc co czytają i jakie książki posiadają, jeszcze lepiej poznasz swoich znajomych i odkryjesz tytuły, które wiesz że chcesz przeczytać.
-    </CommentLeft>
-    <CommentRight style={secondCommentStyle}
-      title="Wzmacniaj więzi ze znajomymi"
-      titleStyle={secondTitleStyle}
-      icon="image/account-switch.png"
-      dividerStyle={rightDividerStyle}
-      textStyle={secondCommentTextStyle}
-      >
-      Lubimy pomagać nawet nieznajomym, ponieważ zawsze wtedy czujemy uczucie wdzięczności, które ma do nas osoba której pomogliśmy.
-      Pożyczając książki naszym przyjaciołom, pokazujemy że nam na nich zależy, przez co i oni z radością będą chcieli pomóc i Tobie.
-    </CommentRight>
+    <hr style={leftDividerStyle} />
+    <table style={leftContainerStyle}>
+      <tr>
+        <td><img src='image/account-search.png' /></td>
+        <td style={titleStyle}>Odbądź podróż po świecie<br/>Twoich znajomych</td>
+        <td rowspan='2'>
+          <div style={cytingStyle}>
+            "Czytanie książki jest, jak podróż<br/>po świecie drugiego człowieka. "<br/>
+            <div style={rightContainerStyle}>
+              Jonathan Carroll
+            </div>
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>
+          Widząc co czytają i jakie książki posiadają, <br />
+          jeszcze lepiej poznasz swoich znajomych <br />
+          i odkryjesz tytuły, które wiesz że chcesz przeczytać.
+        </td>
+      </tr>
+    </table>
+    <hr style={rightDividerStyle} />
+    <table style={rightContainerStyle}>
+      <tr>
+        <td style={titleStyle}>Wzmacniaj więzi ze znajomymi</td>
+        <td><img src='image/account-switch.png' alt=""/></td>
+      </tr>
+      <tr>
+        <td>Lubimy pomagać nawet nieznajomym, ponieważ zawsze wtedy czujemy <br />
+            uczucie wdzięczności, które ma do nas osoba której pomogliśmy.<br />
+            Pożyczając książki naszym przyjaciołom, pokazujemy że nam na nich<br />
+            zależy, przez co i oni z radością będą chcieli pomóc i Tobie.</td>
+        <td></td>
+      </tr>
+    </table>
+    <hr style={leftDividerStyle} />
+    <table style={leftContainerStyle}>
+      <tr>
+        <td><img src='image/buffer.png' /></td>
+        <td style={titleStyle}>Odkryj na nowo, przeczytane przez Ciebie książki</td>
+        <td rowspan="2"><img src='image/photo-reading-guy.png' width="300px" alt=""/></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Dzięki temu że Ty i twój znajomy przeczytaliście tę samą książkę
+            i o tym wiecie, na pewno będzie to okazja dla Was do rozmowy o niej.
+            Ciekawe refleksje i zobaczenie książki z perspektywy innej osoby, to
+            coś dzięki czemu nie tylko zawsze będziesz mieć pasjonujący wspólny
+            temat ze znajomymi, ale także poznasz swoje książki zupełnie od nowa.
+        </td>
+      </tr>
+    </table>
   </div>
-);
+)
 
-export default Comments;
+export default Comments
